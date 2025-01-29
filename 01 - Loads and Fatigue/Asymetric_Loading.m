bcbb12f06473e4340fe2e0b3a4950b4c63b67dd1
Jerry <jerrygao2004@outlook.com>
2025-01-29 15:25:57 +0000

    Added asymmetric loading

---
+++ 01 - Loads and Fatigue/Asymetric_Loading.m
@@ -0,0 +1,13 @@
+Fmax = 286.6e3;
+engine1 = 0.27;
+engine2 = 0.62;
+b = 65;
+ye = b/2*engine2;
+
+xle = 26.5;
+xac = xle + 10.1;
+xe = 31.3;
+xvtac = xac + 30.7;
+dx = xvtac - xe;
+
+Fy = Fmax*ye/dx
